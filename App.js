b523b2410ac5b94a88981595846a8ecd501b6d15
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-03-29 19:57:40 +0700

    update 2

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useFonts } from 'expo-font';
 export default function App() {
+  const [loadFont] = useFonts({
+    EduNS: require('./src/assets/fonts/EduNSWACTFoundation-VariableFont_wght.ttf'),
+    Fasthand: require('./src/assets/fonts/Fasthand-Regular.ttf'),
+  });
+  if (!loadFont) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text2}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text2: {
+    fontSize: 20,
+    fontFamily: 'Fasthand',
+    fontWeight: '600',
+
+  },
+  text1: {
+    fontSize: 20,
+    fontFamily: 'EduNS',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
